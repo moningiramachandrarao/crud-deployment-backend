fdbe20f5dd45877cf8ec6cbae2cb9950290fd4da
Ramachandra Rao Moningi <moningiramachandrarao@gmail.com>
2023-10-25 20:54:06 +0530

    commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const studentRoute=require("./controller/studentRoute");
+const bodyParser=require("body-parser");
+const cors=require("cors");
+const app=express();
+mongoose.set("strictQuery",true);
+
+mongoose.connect("mongodb+srv://test:12345@cluster0.xcr2byu.mongodb.net/schooldb")
+var db=mongoose.connection;
+db.on("open",()=>console.log("connected"));
+db.on("error",()=>console.log("error"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
+app.use("/studentRoute",studentRoute);
+app.listen(4000,()=>{
+    console.log("server started at 4000");
+})
